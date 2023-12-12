258a1889991dd6599fafd97a6a2a0c0d041b5c4a
suchir <ryalisuchir@gmail.com>
2023-12-11 22:58:47 -0600

    updated commands

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/subsystems/Angle.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.common.commandbase.subsystems;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+public class Angle extends SubsystemBase {
+    private final Servo dump;
+
+    public static double intake_position = 0.65;
+    public static double rest_position = 0.6;
+    public static double outtake_position = 0.845;
+
+    public Angle(Servo d) {
+        dump = d;
+    }
+
+    public void in() {
+        dump.setPosition(intake_position);
+    }
+
+    public void rest() {
+        dump.setPosition(rest_position);
+    }
+
+    public void out() {
+        dump.setPosition(outtake_position);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/subsystems/Claw.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.common.commandbase.subsystems;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+public class Claw extends SubsystemBase {
+    private final Servo clawLeft, clawRight;
+    // * can be tuned in dashboard
+    public static double grabPositionLeft = 0.2, grabPositionRight = 0.3; //MODIFY
+    public static double releasePositionLeft = 0.5, releasePositionRight = 0.05; //MODIFY
+
+    public Claw(Servo clawLeft, Servo clawRight) {
+        this.clawLeft = clawLeft;
+        this.clawRight = clawRight;
+    }
+
+    public Command grab() {
+        return new InstantCommand(() -> {
+            clawLeft.setPosition(grabPositionLeft);
+            clawRight.setPosition(grabPositionRight);
+        }, this).andThen(
+                new WaitCommand(500)
+        );
+    }
+
+    public Command release() {
+        return new InstantCommand(() -> {
+            clawLeft.setPosition(releasePositionLeft);
+            clawRight.setPosition(releasePositionRight);
+        }, this).andThen(
+                new WaitCommand(500)
+        );
+    }
+}
